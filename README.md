39d01d02ade55959fced771d0a29590edc65221d
Irdof <irdof321@gmail.com>
2025-12-23 15:56:49 +0100

    Updated readme and added renv

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # SSS-Data-analysis
+
+## R env
+
+### Installing a package
+
+To install a package in your `R env` enter the following in the console:
+
+```r
+install.packages("my_pckg_name")
+```
+
+### Update / freeze the environment (lockfile)
+
+After adding/removing/upgrading packages, update the lockfile:
+
+```r
+renv::snapshot()
+```
+
+### 4) Restore the environment (on a new machine / fresh clone)
+
+After cloning the repo, open the project in RStudio and run:
+
+```r
+renv::restore()
+```
--- analysis.R
+++ analysis.R
@@ -1,8 +1,12 @@
+
+library(lubridate)
+
+
 ####################################################################
 #      CONSTANTS PARAMETERS
 ####################################################################
 data_file <- "sample_survey_results.csv"
-
+REMOVE_NOT_SUB = FALSE # remove rows without a submitted date
 
 
 
@@ -12,6 +16,17 @@ data_file <- "sample_survey_results.csv"
 ####################################################################
 data <- read.csv(data_file, header = TRUE, sep=";")
 
+
+# if remove not submitted data
+print(is.Date(data$submitdate))
+
+####################################################################
+#   Study variables
+####################################################################
+
 # 1. demographics data
+
+
+# table of current year - date of birth where there is an input
 ages <- strtoi(strftime(Sys.Date(),"%Y")) - subset(data$dmbirth, ! is.na(data$dmbirth))
 hist(ages,breaks=100)
